613bd1c705c9114a62a15f099992a47522fb0153
jek <jihoon.kim89@gmail.com>
2020-11-03 13:56:26 +0900

    Add: added logger to settings

--- dev101/settings.py
+++ dev101/settings.py
@@ -163,3 +163,21 @@ CACHES = {
         }
     }
 }
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
