8c20c7cdc93cc8c73cc0d13bcc3406d163d2bca8
ecila7290 <jihoon.kim89@gmail.com>
2020-11-04 14:47:43 +0900

    Edit: 유저 앱 생성 및 수정

--- dev101/settings.py
+++ dev101/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'product',
+    'user',
 ]
 
 MIDDLEWARE = [
---
+++ user/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class User(models.Model):
+    name         = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=300, unique=True)
+    password     = models.CharField(max_length=300)
+    phone_number = models.CharField(max_length=45)
+    image_url    = models.URLField(max_length=2000)
+    is_active    = models.BooleanField(default=False)
+    creator      = models.OneToOneField(Creator, on_delete=models.SET_NULL, null=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+
+class Creator(models.Model):
+    image_url    = models.URLField(max_length=2000)
+    nickname     = models.CharField(max_length=45)
+    introduction = models.TextField()
+
+    class Meta:
+        db_table = 'creators'
