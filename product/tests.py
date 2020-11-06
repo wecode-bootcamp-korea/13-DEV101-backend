caa556b99a8a1950e6b65ff2c9af3b832fe0fd0b
ecila7290 <jihoon.kim89@gmail.com>
2020-11-06 14:12:50 +0900

    Add: added product list & search view & gitignore & model fields

--- product/tests.py
+++ product/tests.py
@@ -1,8 +1,9 @@
 import json
 
+from datetime  import datetime
 from django.test import TestCase, Client, TransactionTestCase
 
-from .models import Product, Category, SubCategory, Coupon, Level, BasicInfo, Introduction, Post, Comment
+from .models import Product, Category, SubCategory, Coupon, Level, BasicInfo, Introduction, Image
 from user.models import Creator, User
 
 class DetailTest(TestCase):
@@ -269,3 +270,236 @@ class CommentTest(TransactionTestCase):
                 'message':'KEY ERROR'
             }
         )
+
+class ProductListTest(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            id=1,
+            name='category1'
+        )
+        SubCategory.objects.create(
+            id=1,
+            category_id=1,
+            name='sub_category1'
+        )
+        Coupon.objects.create(
+            id=1,
+            name='coupon1'
+        )
+        Level.objects.create(
+            id=1,
+            name='level1'
+        )
+        Creator.objects.create(
+            id=1,
+            image_url='www.url.com',
+            nickname='nickname',
+            introduction='hihihi'
+        )
+        Product.objects.create(
+            id=1,
+            name='class1',
+            category_id=1,
+            sub_category_id=1,
+            creator_id=1,
+            level_id=1,
+            coupon_id=1,
+            price=300000,
+            discount=0.55,
+            chapter=20,
+            chapter_detail=33,
+            subtitle_flag=True,
+            is_checked=True,
+            is_open=True
+        )
+        BasicInfo.objects.create(
+            id=1,
+            product_id=1,
+            category_id=1,
+            sub_category_id=1,
+            category_detail='hihi',
+            level_id=1
+        )
+        Introduction.objects.create(
+            id=1,
+            product_id=1,
+            theme_image_url='www.url.com',
+            process_image_url='www.url.com',
+            work_image_url='www.url.com',
+            theme_description='haha',
+            process_description='haha',
+            work_description='haha'
+        )
+        Image.objects.create(
+            id=1,
+            product_id=1,
+            image_url='www.url.com'
+        )
+    
+    def tearDown(self):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+        SubCategory.objects.all().delete()
+        Coupon.objects.all().delete()
+        Level.objects.all().delete()
+        Creator.objects.all().delete()
+        BasicInfo.objects.all().delete()
+        Introduction.objects.all().delete()
+
+    def test_product_list_get_success(self):
+        client=Client()
+        self.maxDiff = None
+        response=client.get('/products')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json(),
+            {
+                "top_10_data": [
+                    {
+                        "product_id": 1,
+                        "image_url": "www.url.com",
+                        "sub_category": "sub_category1",
+                        "mentor": "nickname",
+                        "title": "class1",
+                        "like_count": 0,
+                        "thumbs_up": 0,
+                        "price": "300000.00",
+                        "discount": "0.55",
+                        "coupon": "coupon1"
+                    }
+                ],
+                "planned_data": [],
+                "updated_data": [
+                    {
+                        "product_id": 1,
+                        "image_url": "www.url.com",
+                        "sub_category": "sub_category1",
+                        "mentor": "nickname",
+                        "title": "class1",
+                        "like_count": 0,
+                        "thumbs_up": 0,
+                        "coupon": "coupon1",
+                        "updated_at": str(datetime.now())
+                    }
+                ]
+            }
+            
+        )
+
+    def test_product_list_get_not_found(self):
+        client=Client()
+        response=client.get('/product')
+        self.assertEqual(response.status_code,404)
+
+class ProductSearchTest(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            id=1,
+            name='category1'
+        )
+        SubCategory.objects.create(
+            id=1,
+            category_id=1,
+            name='sub_category1'
+        )
+        Coupon.objects.create(
+            id=1,
+            name='coupon1'
+        )
+        Level.objects.create(
+            id=1,
+            name='level1'
+        )
+        Creator.objects.create(
+            id=1,
+            image_url='www.url.com',
+            nickname='nickname',
+            introduction='hihihi'
+        )
+        Product.objects.create(
+            id=1,
+            name='class1',
+            category_id=1,
+            sub_category_id=1,
+            creator_id=1,
+            level_id=1,
+            coupon_id=1,
+            price=300000,
+            discount=0.55,
+            chapter=20,
+            chapter_detail=33,
+            subtitle_flag=True,
+            is_checked=True,
+            is_open=True
+        )
+        BasicInfo.objects.create(
+            id=1,
+            product_id=1,
+            category_id=1,
+            sub_category_id=1,
+            category_detail='hihi',
+            level_id=1
+        )
+        Introduction.objects.create(
+            id=1,
+            product_id=1,
+            theme_image_url='www.url.com',
+            process_image_url='www.url.com',
+            work_image_url='www.url.com',
+            theme_description='haha',
+            process_description='haha',
+            work_description='haha'
+        )
+        Image.objects.create(
+            id=1,
+            product_id=1,
+            image_url='www.url.com'
+        )
+    
+    def tearDown(self):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+        SubCategory.objects.all().delete()
+        Coupon.objects.all().delete()
+        Level.objects.all().delete()
+        Creator.objects.all().delete()
+        BasicInfo.objects.all().delete()
+        Introduction.objects.all().delete()
+
+    def test_product_search_get_success(self):
+        client=Client()
+        self.maxDiff = None
+        response=client.get('/search?query=class1')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json(),
+            {
+            "data": [
+                {
+                    "product_id": 1,
+                    "image_url": "www.url.com",
+                    "sub_category": "sub_category1",
+                    "mentor": "nickname",
+                    "title": "class1",
+                    "like_count": 0,
+                    "thumbs_up": 0,
+                    "price": "300000.00",
+                    "discount": "0.55",
+                    "coupon": "coupon1"
+                }
+            ]
+        }
+            
+        )
+    def test_product_search_get_fail(self):
+        client=Client()
+        response=client.get('/search?sort=4')
+        self.assertEqual(response.status_code,400)
+        self.assertEqual(response.json(),
+            {
+                'message':'WRONG SORTING'
+            }
+        )
+
+    def test_product_search_get_not_found(self):
+        client=Client()
+        response=client.get('/search/class1')
+        self.assertEqual(response.status_code,404)
--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import DetailView, CommentView
+from .views import DetailView, CommentView, ProductsView, SearchView
 
 urlpatterns = [
     path('product/<int:product_id>', DetailView.as_view()),
     path('product/<int:product_id>/post/<int:post_id>', CommentView.as_view()),
+    path('products', ProductsView.as_view()),
+    path('search',SearchView.as_view()),
 ]
