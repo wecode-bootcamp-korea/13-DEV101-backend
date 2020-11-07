f2688fa9bcc95ed1cbe8ff660add57c56e6c701f
ecila7290 <jihoon.kim89@gmail.com>
2020-11-07 15:14:42 +0900

    Add: added product like view3 & gitignore & edited url & product like view, comment view & resolved conflict

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import DetailView, CommentView, ProductsView, SearchView
+from .views import DetailView, CommentView, ProductLikeView, ProductsView, SearchView
 
 urlpatterns = [
     path('product/<int:product_id>', DetailView.as_view()),
     path('product/<int:product_id>/post/<int:post_id>', CommentView.as_view()),
     path('products', ProductsView.as_view()),
     path('search',SearchView.as_view()),
+    path('product/<int:product_id>/like', ProductLikeView.as_view()),
 ]
