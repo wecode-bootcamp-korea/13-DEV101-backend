55a0f852bd27182ac170ee0ee7f44393e21546a1
ecila7290 <jihoon.kim89@gmail.com>
2020-11-09 20:22:31 +0900

    Edit: edited user model

--- user/models.py
+++ user/models.py
@@ -17,16 +17,16 @@ class Creator(models.Model):
 class User(models.Model):
     name            = models.CharField(max_length=45)
     email           = models.EmailField(max_length=300, unique=True)
-    password        = models.CharField(max_length=300)
-    phone_number    = models.CharField(max_length=45)
-    image_url       = models.URLField(max_length=2000)
+    password        = models.CharField(max_length=300, null=True)
+    phone_number    = models.CharField(max_length=45, null=True)
+    image_url       = models.URLField(max_length=2000, null=True)
     is_active       = models.BooleanField(default=False)
     creator         = models.OneToOneField(Creator, on_delete=models.SET_NULL, null=True)
     created_at      = models.DateTimeField(auto_now_add=True)
     updated_at      = models.DateTimeField(auto_now=True)
     cheer_point     = models.PositiveIntegerField(default=10)
     social          = models.ForeignKey(SocialPlatform, on_delete=models.CASCADE, null=True)
-    social_login_id = models.EmailField(max_length=200, null=True)
+    social_login_id = models.CharField(max_length=200, null=True)
     coupon          = models.ManyToManyField('product.Coupon', through='user.UserCoupon', related_name='user')
 
     class Meta:
